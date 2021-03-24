74fa6272de4e2a64d8db5cdb2e3b6b65352b496b
Федоров Евгений Андреевич <Viva_la_lumiere@mail.ru>
2021-03-24 19:15:26 +0700

    Первая программа и комментарии

--- Program.cs
+++ Program.cs
@@ -6,13 +6,13 @@ namespace MyFirstApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.Write(" Type your name: ");
-            string name = Console.ReadLine();
-            Console.WriteLine("Hello "+ name + "!");
-            Console.Read();
-            Console.Clear();
-            Console.ReadLine();
+            Console.WriteLine("Hello World!"); // с переносом на след строку
+            Console.Write(" Type your name: "); // без переноса
+            string name = Console.ReadLine(); // чтение имени в память
+            Console.WriteLine("Hello "+ name + "!"); // новая строка + переменная
+            Console.Read(); // считывает только один символ 
+            Console.Clear(); // очистка
+            Console.ReadLine(); // 
         }
     }
 }
