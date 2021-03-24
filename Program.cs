9cdd3873611a0a255e189e56587c5c567b2cc290
Федоров Евгений Андреевич <Viva_la_lumiere@mail.ru>
2021-03-24 19:12:07 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyFirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Write(" Type your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello "+ name + "!");
+            Console.Read();
+            Console.Clear();
+            Console.ReadLine();
+        }
+    }
+}
